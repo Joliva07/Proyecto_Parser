0a3428ffcca73a1c37b4940f24a521db8ea4a3f9
Josue Oliva <josu49067@gmail.com>
2023-10-05 21:16:24 -0600

    V1.3.0

--- build_parser.bat
+++ build_parser.bat
@@ -1,18 +1,18 @@
-rem Bison command
-rem bison -d Parser.y
+::rem Bison command
+::rem bison -d Parser.y
 
-rem Flex command
-rem flex Parser.l
+::rem Flex command
+::rem flex Parser.l
 
-rem GCC commands
-rem gcc -c lex.yy.c -o lex.yy.o
-rem gcc -c Parser.tab.c -o Parser.tab.o
+::rem GCC commands
+::rem gcc -c lex.yy.c -o lex.yy.o
+::rem gcc -c Parser.tab.c -o Parser.tab.o
 
-rem Linking
-rem gcc lex.yy.o Parser.tab.o -o myParser.exe
+::rem Linking
+::rem gcc lex.yy.o Parser.tab.o -o myParser.exe
 
-rem Solicitar al usuario la ruta del archivo XML a ejecutar
+::rem Solicitar al usuario la ruta del archivo XML a ejecutar
 set /p xmlFilePath=Ingrese la ruta del archivo XML que desea ejecutar: 
 
-rem Ejecutar el parser con el archivo XML proporcionado por el usuario
+::rem Ejecutar el parser con el archivo XML proporcionado por el usuario
 myParser.exe "%xmlFilePath%"
--- run.bat
+++ run.bat
@@ -1,19 +1,19 @@
-rem Bison command
-rem bison -d Parser.y
+::rem Bison command
+::rem bison -d Parser.y
 
-rem Flex command
-rem flex Parser.l
+::rem Flex command
+::rem flex Parser.l
 
-rem GCC commands
-rem gcc -c lex.yy.c -o lex.yy.o
-rem gcc -c Parser.tab.c -o Parser.tab.o
+::rem GCC commands
+::rem gcc -c lex.yy.c -o lex.yy.o
+::rem gcc -c Parser.tab.c -o Parser.tab.o
 
-rem Linking
-rem gcc lex.yy.o Parser.tab.o -o myParser.exe
+::rem Linking
+::rem gcc lex.yy.o Parser.tab.o -o myParser.exe
 
-rem clear (comentario)
-rem Run the parser
-rem myParser.exe Ejemplo.xml ---
+::rem clear (comentario)
+::rem Run the parser
+::rem myParser.exe Ejemplo.xml ---
 
 @echo off
 echo Compilando el programa C++...
